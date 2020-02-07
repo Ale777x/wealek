b434720cc6b6a4b2949c6670e1912b7c9dad340e
Ale777x <alexcarpovmd@gmail.com>
2020-02-07 09:06:36 +0200

    cr7

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,7 +6,14 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function index(){
-    	return view ('index');
-    } 
+  
+
+public function index(){
+        $hw='Hello world!';
+        $ms='This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
+        return view('index')->with(['hw'=>$hw, 'ms'=>$ms]);
+    
+}
+
+         
 }
